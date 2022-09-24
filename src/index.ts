9d6940796d0209fa37097157741fb27ed692bc37
Pcrab <pcrab@pcrab.xyz>
2022-09-24 23:06:25 +0800

    delete unused files

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { CustomLocalStorage } from "./storage.js";
-
-export const lstorage = new CustomLocalStorage("local");
-export const sstorage = new CustomLocalStorage("session");
-export { CustomLocalStorage, prefix } from "./storage.js";
